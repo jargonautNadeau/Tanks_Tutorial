3003befc3e8703ee98e027d89107debd0273c33e
David Nadeau <nadeaud.d@gmail.com>
2018-07-17 12:00:13 -0700

    Linked scenes together although they are not responsive

---
+++ Assets/Scripts/UI/MenuManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+
+	public void LaunchLocalLobby(){
+		
+		SceneManager.LoadScene ("TwoPlayerLobby");
+	}
+
+	public void LaunchOnlineLobby(){
+		SceneManager.LoadScene ("OnlineLobby");
+	}
+
+	public void ExitGame(){
+		Application.Quit ();
+	}
+	public void StartLocalGame(){
+		SceneManager.LoadScene ("main");
+	}
+	public void BackToMainMenu(){
+		SceneManager.LoadScene ("Menu");
+	}
+}
